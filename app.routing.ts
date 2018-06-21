af9e72d239817c829e169091f56867110e5c7cf2
Adriana Díaz <adriana.diaz.gonzalez4@gmail.com>
2018-06-21 15:44:34 -0500

    vista login
    vista login

--- app.routing.ts
+++ app.routing.ts
@@ -5,8 +5,12 @@ import { Routes } from "@angular/router";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
 
+import { LoginComponent} from "./login/login.component";
+
+import { from } from "rxjs/internal/observable/from";
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
+    { path: "", redirectTo: "/login", pathMatch: "full" },
+    { path: "login", component:LoginComponent},  
     { path: "items", component: ItemsComponent },
     { path: "item/:id", component: ItemDetailComponent },
 ];
