68857c0639ee5036ed695e87b852492ec7757f76
Israel <mbi1204@hotmail.com>
2018-06-28 12:05:07 -0500

    cambios login
    cambios login

--- login/login.component.ts
+++ login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { LoginService } from "../service/login.service";
 import {Router,ActivatedRoute,Params} from '@angular/router';
-import 'nativescript-localstorage';
+import {ctEmpleado} from "../model/ctEmpleado";
+import { SESSION } from '../service/global';
+import * as dialogs from "ui/dialogs";
+
 
 @Component({
   selector: "ns-login",
@@ -13,6 +16,8 @@ import 'nativescript-localstorage';
 export class LoginComponent implements OnInit {
   public usuario: string;
   public password: string;
+  public _ctEmpleado :ctEmpleado;
+  
   constructor(private _Service: LoginService, 
               private _route:ActivatedRoute,
               private _router:Router) {}
@@ -22,34 +27,62 @@ export class LoginComponent implements OnInit {
   }
 
   login(): void {
-      var resultado, Error, Mensaje ;
+      var resultado, error, mensaje,tt_ctEmpleado;
 
-      
-    localStorage.clear();
 
-    localStorage.setItem('session', 'Adriana diaz gonzalez');
-
-  
-
-    this._router.navigate(["/mesas"]);
-
-    /*console.log('login()->'  + 'Usuario:' + this.usuario + 'Password' + this.password ); 
+    console.log('login()->'  + 'Usuario:' + this.usuario + 'Password' + this.password ); 
     this._Service.PostLogin(this.usuario, this.password).subscribe(
       result => {
         console.log(result);
 
-        resultado = result.body;
-        Error   = resultado.response.oplError;
-        Mensaje = resultado.response.opcMensage;
+        resultado = result.body;        
+        error     = resultado.response.oplError;
+        mensaje   = resultado.response.opcMensage;
+        
 
-        console.log("resultado" +  resultado);
+        if (error == 'true'){
+          dialogs.alert(mensaje).then(()=> {
+            console.log(mensaje);
+          });
 
-
-        if (Error == "true"){
-            alert(Mensaje);
         }else {
-            this._router.navigate(["/home"]);
-        }        
+          tt_ctEmpleado = resultado.response.tt_ctEmpleado.tt_ctEmpleado[0];
+
+
+          this._ctEmpleado = new ctEmpleado
+          (   tt_ctEmpleado.cCveCia ,
+              tt_ctEmpleado.iEmpleado,
+              tt_ctEmpleado.cCveEmp ,
+              tt_ctEmpleado.cPassword,
+              tt_ctEmpleado.cNombre,
+              tt_ctEmpleado.cDireccion,
+              tt_ctEmpleado.cTelefono1,
+              tt_ctEmpleado.cTelefono2,
+              tt_ctEmpleado.iPuestoEmpID,
+              tt_ctEmpleado.cEmail,
+              tt_ctEmpleado.cApellidoP,
+              tt_ctEmpleado.cApellidoM,
+              tt_ctEmpleado.bImagen,
+              tt_ctEmpleado.cFoto,
+              tt_ctEmpleado.lActivo,
+              tt_ctEmpleado.dtIngreso,
+              tt_ctEmpleado.cUsuario,
+              tt_ctEmpleado.dtCreado,
+              tt_ctEmpleado.dtModificado
+
+        );
+        
+      
+        if ( this._ctEmpleado == null) {
+          dialogs.alert("error en la carga de la session del empleado").then(()=> {
+            console.log(mensaje);
+          });
+        } else {        
+           SESSION.g_ctEmpleado = this._ctEmpleado;        
+           this._router.navigate(["/home"]);
+        } 
+      }        
+               
       },
       error => {
         console.log(error);
@@ -63,6 +96,6 @@ export class LoginComponent implements OnInit {
             error.error._errors[0]._errorNum
         );
       }
-    );*/
+    );
   }
 }
