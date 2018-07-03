5d9dacb0d4c463370c98cacc660974e785607e88
Israel <mbi1204@hotmail.com>
2018-07-03 16:48:14 -0500

    ok
    ok

---
+++ service/vtOrden.service.ts
@@ -0,0 +1,49 @@
+/*
+autor: imendoza@sinergitec.com
+fecha: 02/07/2018
+obs  : servicio relacionado al login
+*/
+import {Injectable} from '@angular/core';
+import { Observable as RxObservable } from "rxjs";
+import { HttpClient, HttpHeaders, HttpResponse ,HttpParams } from "@angular/common/http";
+import {GLOBAL} from './global';
+import {vtOrden} from '../model/vtOrden'; 
+
+@Injectable()
+export class vtOrdenService {
+    public url: string;
+
+    constructor(public _http: HttpClient) {
+        this.url = GLOBAL.URL;
+    }
+
+    /**
+     * Crear una orden
+     */
+    PostCrearOrden(usuario: string , _vtOrden: vtOrden){
+
+        var tt_Nuevos = [_vtOrden];
+
+          // set headers here e.g.
+          let headers = new HttpHeaders({
+            "AuthKey": "",
+            "AuthToken": "",
+            "Content-Type": "application/json",
+         });
+
+        // peticion 
+        let request  =   JSON.stringify({
+            "request":
+            {
+                "ipcUsuario": usuario,
+                "ds_Nuevos": { tt_Nuevos }              
+                
+            }
+          });
+
+          console.log(request);
+        
+          return this._http.post(this.url + 'vtOrden',  request , {observe: 'response' ,  headers: headers });
+
+    }
+}
