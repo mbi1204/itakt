2264496ba461df4cf25f821b77d9fc59583c9a27
Andros Hernandez <androshernandez@sinergitec.mx>
2018-07-18 10:30:20 -0500

    cambio en mesas
    cambio en mesas

--- mesas/mesas.component.ts
+++ mesas/mesas.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnInit } from "@angular/core";
 import {ctMesaService} from '../service/mesas.service'; 
 import {ctMesas} from '../model/ctMesas';
+import {SESSION} from '../service/global';
+
+import {ctMesaHService} from '../service/mesasHist.service'; 
+import {vtMesaHist} from '../model/vtMesaHist';
+import { Background } from "tns-core-modules/ui/styling/background";
+import { backgroundColorProperty } from "tns-core-modules/ui/page/page";
 
 @Component({
     selector: "ns-listaMesas",
     moduleId: module.id,
     templateUrl: "./mesas.component.html",
-    providers: [ctMesaService]
+   // styleUrls: ["./mesas.component.css"],
+    providers: [ctMesaService, ctMesaHService]
 })
 export class MesasComponent implements OnInit {
     public _ctMesasObj:ctMesas;
     public _ctMesasArray:Array<ctMesas> = [];
-   
-    constructor( private _Service: ctMesaService) { }    
+    public _ctMesasHObj:vtMesaHist;
+    public _ctMesasHArray:Array<vtMesaHist> = [];
+    
 
+    constructor( private _Service: ctMesaService, private _ServiceH: ctMesaHService) {
+       
+
+    }    
+
+    refreshMesa(){
+        window.location.reload();
+    }
     ngOnInit():void{
-        var respuesta     : any;
-        var lista : any  ;
+        console.log("mesas.component.ts");
+        var respuesta: any;
+        var lista: any  ;
         var Error: any;
-        var Mensaje : any;
+        var Mensaje: any;
+
+        
+        
 
         this._Service.getctMesas("mfeliz", "true").subscribe((result) => { 
             respuesta = result.body;
@@ -60,7 +80,82 @@ export class MesasComponent implements OnInit {
         }, (error) => {
             console.log("result");
             console.log(error);
+        });
+
+        
+        
+        this._ServiceH.getctMesasH("mfeliz",  1 ).subscribe((result) => {    
+            respuesta = result.body;
+            console.log("men hist: ->" + respuesta);
+            Error   = respuesta.response.oplError;
+            Mensaje = respuesta.response.opcMensage;
+
+            if (Error == 'true'){
+                alert(Mensaje);
+            }else {
+                lista = respuesta.response.tt_vtMesaHist.tt_vtMesaHist;
+                
+                lista.forEach(itemH=>{ 
+                    this._ctMesasHObj = new vtMesaHist(
+                    itemH.cCveCia,
+                    itemH.iSucursal,
+                    itemH.iMesa,
+                    itemH.iPartida,
+                    itemH.dtFechaHist,
+                    itemH.iEstatusMesa,
+                    itemH.cUsuario,
+                    itemH.dtCreado,
+                    itemH.dtModificado,
+            ); 
+            this._ctMesasHArray.push(this._ctMesasHObj
+            );
+        }); 
+        
+        }
+        
+        }, (error) => {
+            console.log("result");
+            console.log(error);
             
-         });
+        });
+          
     }
+    regresaMesa(mesa:string): void {
+        SESSION.g_cMesa=mesa;
+        console.log("valor en session: --> " + SESSION.g_cMesa);
+    }
+
+   
+    pintaBoton(imesa){
+        {
+            var viMesa: any;
+            viMesa = this._ctMesasHArray.find(cMesa=>cMesa.iMesa == imesa);
+            
+            switch (viMesa.iEstatusMesa) {
+                case 1:{
+                    let myStyles = {
+                        'background-color': 'red', 
+                             
+                    };
+                return myStyles;
+                }
+                case 2:{
+                    let myStyles = {
+                        'background-color': 'gray',
+                        
+                    };
+                return myStyles;
+                }
+                case 3: {
+                    let myStyles = {
+                        'background-color': 'greenyellow', 
+                        
+                    };
+                return myStyles;
+                }
+            }
+        }   
+    }
+    
+   
 }
